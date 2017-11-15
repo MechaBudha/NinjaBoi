f96ec2a7cf2639381b0fa107f904ac8a7e5850ad
Unknown <tmugetti@gmail.com>
2017-11-15 15:29:40 -0300

    weitas

--- NinjaBoi/source/SandBoxState.hx
+++ NinjaBoi/source/SandBoxState.hx
@@ -1,6 +1,8 @@
 package;
 
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.tile.FlxTilemap;
 
 /**
  * ...
@@ -8,10 +10,15 @@ import flixel.FlxState;
  */
 class SandBoxState extends FlxState 
 {
-
+	private var _map:FlxOgmoLoader;
+	private var _mPiso:FlxTilemap;
+	private var _mPlat:FlxTilemap;
+	
 	public function new() 
 	{
-		
+		_map = new FlxOgmoLoader(AssetPaths.sandbox__oep);
+		_mPiso = _map.loadTilemap(AssetPaths.tilesPetes__png,16,16,"Piso");
+		_mPlat = _map.loadTilemap(AssetPaths.tilesPetes__png,16,16,"Plataforma");
 	}
 	
 }
